0bf41915844b229ad8b4453b615dd1ec73fed8a4
Glitch (hello-webpage) <none>
2020-06-01 17:01:04 +0000

    🎡🐀 Checkpoint ./index.html:319521/1310 ./script.js:319521/4476

--- script.js
+++ script.js
@@ -11,6 +11,27 @@ const populateCode = exNum => {
   cssArea.style.display = "block";
 };
 
+// Events
+
+[...document.querySelectorAll(".tab-area button")].forEach(button => {
+  button.addEventListener("click", event => {
+    let lang = event.target.innerText.toLowerCase();
+    const exNum = event.target.parentElement.parentElement.parentElement.classList[1];
+    if (lang === "css") {
+      document.querySelector(`.${exNum} .code-container.html`).style.display = "none";
+      document.querySelector(`.${exNum} .tab.html`).classList.remove("active");
+      document.querySelector(`.${exNum} .code-container.css`).style.display = "block";
+      document.querySelector(`.${exNum} .tab.css`).classList.add("active");
+    } else {
+      document.querySelector(`.${exNum} .code-container.css`).style.display = "none";
+      document.querySelector(`.${exNum} .tab.css`).classList.remove("active");
+      document.querySelector(`.${exNum} .code-container.html`).style.display = "block";
+      document.querySelector(`.${exNum} .tab.html`).classList.add("active");
+    }
+  });
+});
+
+
 // Ex1: Absolutely Centered
 
 const ex1 = {
@@ -64,25 +85,30 @@ const ex2 = {
   }`
 };
 
-// Events
-
-[...document.querySelectorAll(".tab-area button")].forEach(button => {
-  button.addEventListener("click", event => {
-    let lang = event.target.innerText.toLowerCase();
-    const exNum = event.target.parentElement.parentElement.parentElement.classList[1];
-    if (lang === "css") {
-      document.querySelector(`.${exNum} .code-container.html`).style.display = "none";
-      document.querySelector(`.${exNum} .tab.html`).classList.remove("active");
-      document.querySelector(`.${exNum} .code-container.css`).style.display = "block";
-      document.querySelector(`.${exNum} .tab.css`).classList.add("active");
-    } else {
-      document.querySelector(`.${exNum} .code-container.css`).style.display = "none";
-      document.querySelector(`.${exNum} .tab.css`).classList.remove("active");
-      document.querySelector(`.${exNum} .code-container.html`).style.display = "block";
-      document.querySelector(`.${exNum} .tab.html`).classList.add("active");
-    }
-  });
-});
+const ex3 = {
+  html: `
+  <div class="parent">
+    <div class="sidebar pink" contenteditable>
+    Min: 150px / Max: 25%
+    </div>
+    <div class="content blue" contenteditable>
+      This element takes the second grid position (1fr), meaning
+      it takes up the rest of the remaining space.
+      <br></br>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+      Omnis nulla architecto maxime modi nisi. Quas saepe dolorum, 
+      architecto quia fugit nulla! Natus, iure eveniet ex iusto 
+      tempora animi quibusdam porro?
+    </div>
+  </div>
+  `,
+  css: `
+  .ex3 .parent {
+    display: grid;
+    grid-template-columns: minmax(150px, 25%) 1fr;
+  }`
+};
 
 populateCode("ex1");
 populateCode("ex2");
+populateCode("ex3");
